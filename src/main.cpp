7db561d88177f498f02f045de36ac58084ab1573
daniellerozenblit <daniellerozenblit@gmail.com>
2022-04-24 21:17:51 -0400

    working writing vol file

--- src/main.cpp
+++ src/main.cpp
@@ -4,17 +4,21 @@
 #include <cstdlib>
 #include <ctime>
 #include <iostream>
+#include "rendering/rendering.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    Rendering r;
+
+    r.test();
 
     srand (static_cast <unsigned> (time(0)));
     // We cannot use w.showFullscreen() here because on Linux that creates the
     // window behind all other windows, so we have to set it to fullscreen after
     // it has been shown. 
-    w.show();
+    //    w.show();
     //w.setWindowState(w.windowState() | Qt::WindowFullScreen); // Comment out this line to have a windowed 800x600 game on startup.
 
     return a.exec();
--- src/rendering/rendering.cpp
+++ src/rendering/rendering.cpp
@@ -4,27 +4,28 @@ void Rendering::write_vol(const std::string &file, std::vector<std::vector<std::
     std::ofstream f;
     f.open(file, std::ios::out | std::ios::binary);
 
-    if(!f) {
+    if(!f.is_open()) {
           std::cout << "ERROR: Cannot open output file" << std::endl;
+          return;
     }
 
     int x_size = values[0].size();
     int y_size = values[1].size();
     int z_size = values[2].size();
 
-    int vol[4] = {'V', 'O', 'L'};
+    char vol[3] = {'V', 'O', 'L'};
     f.write((char*) &vol, sizeof(vol));
 
-    int version[1] = {3}; // File format version number
+    uint8_t version = 3; // File format version number
     f.write((char*) &version, sizeof(version));
 
-    int type[1] = {1}; // Encoding identifier (float32).
+    int32_t type = 1; // Encoding identifier (float32).
     f.write((char*) &type, sizeof(type));
 
-    int size[3] = {x_size, y_size, z_size}; // Number of cells in each dimension
+    int32_t size[3] = {x_size, y_size, z_size}; // Number of cells in each dimension
     f.write((char*) &size, sizeof(size));
 
-    int channels[1] = {1}; // Number of channels
+    int32_t channels = 1; // Number of channels
     f.write((char*) &channels, sizeof(channels));
 
     float bbox[6] = {0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f};
@@ -35,12 +36,13 @@ void Rendering::write_vol(const std::string &file, std::vector<std::vector<std::
               for (int z = 0; z < z_size; ++z) {
                     {
                       float data = values[x][y][z];
-                      f.write(reinterpret_cast<char *>(&data), sizeof(data));
+                      f.write((char*) &data, sizeof(float));
                     }
               }
         }
     }
     f.close();
+    std::cout << "Wrote to vol file" << std::endl;
 }
 
 float sigmat_f(int x, int y, int z) {
@@ -55,17 +57,18 @@ void Rendering::test() {
     std::vector<std::vector<std::vector<float>>> sigmat;
 
     for (int x = 0; x < res; ++x) {
-        std::vector<float> v_x;
-
+        std::vector< std::vector<float>> v_x;
         for (int y = 0; y < res; ++y) {
             std::vector<float> v_y;
 
-              for (int z = 0; z < res; ++z) {
-                    {
-                      v_y.push_back(sigmat_f(x / res, y / res, z / res));
-                    }
-              }
+            for (int z = 0; z < res; ++z) {
+                v_y.push_back(sigmat_f(x / res, y / res, z / res));
+            }
+
+            v_x.push_back(v_y);
         }
+
+        sigmat.push_back(v_x);
     }
-    write_vol("textures/sigmat.vol", sigmat);
+    write_vol("./sigmat.vol", sigmat);
 }
