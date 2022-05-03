3aab26c36481cd0e4f9c50058f954f1260ac6ae7
J. Alex Mina <juanmina1200@gmail.com>
2022-05-03 11:34:50 -0400

    push
    a

--- src/simulation.cpp
+++ src/simulation.cpp
@@ -72,22 +72,22 @@ void Simulation::draw(Shader *shader, Shader *m_normalsShader, Shader *m_normals
     }
 
 
-//    for(std::vector<Shape> a : densitySpheres)
-//    {
-//        for(Shape s: a )
-//        {
-//            s.m_red = (float)a.size()/(float)MAXDENSITYSPHERES;
-//            s.m_green = (1.f-(((float)a.size()/(float)MAXDENSITYSPHERES)))-0.5;
-//            if(s.m_green < 0)
-//            {
-//                s.m_green = 0;
-//            }
-//            s.m_blue = 0.f;
-//            s.m_wireframe = true;
-//            s.draw(shader, false, 1.f);
+    for(std::vector<Shape> a : densitySpheres)
+    {
+        for(Shape s: a )
+        {
+            s.m_red = (float)a.size()/(float)MAXDENSITYSPHERES;
+            s.m_green = (1.f-(((float)a.size()/(float)MAXDENSITYSPHERES)))-0.5;
+            if(s.m_green < 0)
+            {
+                s.m_green = 0;
+            }
+            s.m_blue = 0.f;
+            s.m_wireframe = true;
+            s.draw(shader, false, 1.f);
 
-//        }
-//    }
+        }
+    }
 
     for(Shape _arrow: arrows)
     {
@@ -100,23 +100,6 @@ void Simulation::draw(Shader *shader, Shader *m_normalsShader, Shader *m_normals
     }
 
     shader->unbind();
-
-
-
-//    m_normalsShader->bind();
-//    for(Shape s : voxels)
-//    {
-//        s.draw(m_normalsShader, true, s.alpha);
-//    }
-//    m_normalsShader->unbind();
-
-//    m_normalsArrowShader->bind();
-//    for(Shape s : voxels)
-//    {
-//        s.draw(m_normalsArrowShader, true, s.alpha);
-//    }
-
-//    m_normalsArrowShader->unbind();
 
 }
 
@@ -159,10 +142,21 @@ void Simulation::initGround()
     m_colliders.push_back(m_ground_collider);
 }
 
+Eigen::Affine3d create_rotation_matrix(double ax, double ay, double az) {
+  Eigen::Affine3d rx =
+      Eigen::Affine3d(Eigen::AngleAxisd(ax, Eigen::Vector3d(1, 0, 0)));
+  Eigen::Affine3d ry =
+      Eigen::Affine3d(Eigen::AngleAxisd(ay, Eigen::Vector3d(0, 1, 0)));
+  Eigen::Affine3d rz =
+      Eigen::Affine3d(Eigen::AngleAxisd(az, Eigen::Vector3d(0, 0, 1)));
+  return rz * ry * rx;
+}
+
+
 void Simulation::initSphere()
 {
 
-    int voxNum = 3;
+    int voxNum = 6;
 
     for(int i = 0; i < voxNum; i++)
     {
@@ -170,8 +164,14 @@ void Simulation::initSphere()
         {
             for(int k = 0; k < voxNum; k++)
             {
+                Eigen::Vector3f normal = {(float)rand()/RAND_MAX - 0.5, (float)rand()/RAND_MAX - 0.5, (float)rand()/RAND_MAX - 0.5};
+                int density_amt = rand() % MAXDENSITYSPHERES;
+
+
+
                 Shape voxel;
                 voxel.alpha = 1.0f;
+
 
                 std::vector<Vector3d> vertices;
                 std::vector<Vector4i> tets;
@@ -215,7 +215,7 @@ void Simulation::initSphere()
 
 
                 std::vector<Shape> desityS;
-                int density_amt = rand() % MAXDENSITYSPHERES;
+
                 float scale = 25.f;
                 Eigen::Vector3d offset = {0,0,0};
 
@@ -279,7 +279,7 @@ void Simulation::initSphere()
                 std::vector<Eigen::Vector3d> pos;
                 for(size_t n = 0; n < arrowData.size(); n+=3)
                 {
-                    Eigen::Vector3d l = {arrowData[n]+i,arrowData[n+1]+j,arrowData[n+2]+k};
+                    Eigen::Vector3d l = {arrowData[n],arrowData[n+1],arrowData[n+2]};
                     pos.push_back(l);
                 }
 
@@ -300,23 +300,48 @@ void Simulation::initSphere()
 
                 std::vector<Eigen::Vector3i> STEMtriangles;
                 std::vector<Eigen::Vector3d> STEMpos;
-                STEMpos.push_back({0.0+i,0.0+j,0.0+k});
-                STEMpos.push_back({0.0+i,-0.1+j,0.0+k});
+                STEMpos.push_back({0.0,0.0,0.0});
+                STEMpos.push_back({0.0,-0.1,0.0});
                 STEMtriangles.push_back({0,1,0});
                 stem.init(STEMpos, STEMtriangles);
 
 
+
+                float pitch = asin(-normal[1]);
+                float yaw = atan2(normal[0], normal[2]);
 
 
                 // Mesh translation
                 Affine3d t = Affine3d(Translation3d(0, 0, 0));
 
                 // Mesh rotation
-                Eigen::AngleAxisd rollAngle(45.0, Eigen::Vector3d::UnitX());
-                Eigen::AngleAxisd pitchAngle(0.0, Eigen::Vector3d::UnitY());
-                Eigen::AngleAxisd yawAngle(0.0, Eigen::Vector3d::UnitZ());
+                Eigen::AngleAxisd rollAngle(0.0, Eigen::Vector3d::UnitX());
+                Eigen::AngleAxisd pitchAngle(pitch, Eigen::Vector3d::UnitY());
+                Eigen::AngleAxisd yawAngle(yaw, Eigen::Vector3d::UnitZ());
                 Eigen::Quaterniond q = yawAngle * pitchAngle * rollAngle;
+                Translation<float,3>(i, j, k).translation();
+                t.translate(Translation3d(i, j, k).translation());
                 t.rotate(q);
+
+                //Translation3d m = Translation3d(i, j, k);
+                //t.translate(m);
+
+//                //Eigen::Matrix3d R;
+//                // Find your Rotation Matrix
+//                Eigen::Vector3d T = {i,j,k};
+//                // Find your translation Vector
+//                Eigen::Matrix4d Trans; // Your Transformation Matrix
+//                Trans.setIdentity();   // Set to Identity to make bottom row of Matrix 0,0,0,1
+//                Trans.block<3,3>(0,0) = t;
+//                Trans.block<3,1>(0,3) = T;
+
+
+//                Eigen::Affine3d r = create_rotation_matrix(1.0, 1.0, 1.0);
+//                Eigen::Affine3d t(Eigen::Translation3d(Eigen::Vector3d(1,1,2)));
+
+//                Eigen::Matrix4d m = (t * r).matrix();
+
+
 
 
 
