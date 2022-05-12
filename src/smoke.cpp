21faa6d09a8ca6b143aad11c5195ac3acdc01f3b
zachmothner <zachary.mothner@gmail.com>
2022-05-12 13:19:33 -0400

    ok advectionn

--- src/smoke.cpp
+++ src/smoke.cpp
@@ -12,17 +12,17 @@ Smoke::Smoke()
 
 void Smoke::update() {
     std::cout << "update" << std::endl;
-//    for (int i = 0; i < SIZE_X; i++) {
-//        for (int j = 0; j < SIZE_Y; j++) {
-//            for (int k = 0; k < SIZE_Z; k++) {
-//                if (getVelocity(Vector3d((i + 0.5) * VOXEL_SIZE, (j + 0.5) * VOXEL_SIZE, (k + 0.5) * VOXEL_SIZE)).norm() > 0) {
-//                    std::cout << "CELL: " << i << ", " << j << ", " << k << " has velocity: " << getVelocity(Vector3d((i + 0.5) * VOXEL_SIZE, (j + 0.5) * VOXEL_SIZE, (k + 0.5) * VOXEL_SIZE)) << std::endl;
-//                } if (getVal(DENSITY, i, j, k) > 0) {
-//                    std::cout << "CELL: " << i << ", " << j << ", " << k << " has density: " << getVal(DENSITY, i, j, k) << std::endl;
-//                }
-//            }
-//        }
-//    }
+    for (int i = 0; i < SIZE_X; i++) {
+        for (int j = 0; j < SIZE_Y; j++) {
+            for (int k = 0; k < SIZE_Z; k++) {
+                if (getVelocity(Vector3d((i + 0.5) * VOXEL_SIZE, (j + 0.5) * VOXEL_SIZE, (k + 0.5) * VOXEL_SIZE)).norm() > 0) {
+                    std::cout << "CELL: " << i << ", " << j << ", " << k << " has velocity: " << getVelocity(Vector3d((i + 0.5) * VOXEL_SIZE, (j + 0.5) * VOXEL_SIZE, (k + 0.5) * VOXEL_SIZE)) << std::endl;
+                } if (getVal(DENSITY, i, j, k) != 0) {
+                    std::cout << "CELL: " << i << ", " << j << ", " << k << " has density: " << getVal(DENSITY, i, j, k) << std::endl;
+                }
+            }
+        }
+    }
     advectVelocity();
     calculateForces();
     advectDensity();
