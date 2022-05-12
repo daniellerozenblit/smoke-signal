7046128291fed29bbdc7b523b39a6d214af917f4
anna finkelstein <anna_finkelstein1@brown.edu>
2022-05-11 21:42:54 -0400

    restructuring mac and const

---
+++ src/grid/macgrid.h
@@ -0,0 +1,32 @@
+#ifndef MACGRID_H
+#define MACGRID_H
+#include <Eigen/StdVector>;
+#include "constants.h";
+
+using namespace Eigen;
+class MACgrid
+{
+public:
+    MACgrid();
+    void initCellData();
+    void initFaceData();
+
+protected:
+    // VOXEL DATA
+    std::vector<std::vector<std::vector<double>>> center_vel_x;
+    std::vector<std::vector<std::vector<double>>> center_vel_y;
+    std::vector<std::vector<std::vector<double>>> center_vel_z;
+
+    std::vector<std::vector<std::vector<double>>> temperature;
+    std::vector<std::vector<std::vector<double>>> density;
+
+
+    //FACE DATA
+    std::vector<std::vector<std::vector<double>>> face_vel_x;
+    std::vector<std::vector<std::vector<double>>> face_vel_y;
+    std::vector<std::vector<std::vector<double>>> face_vel_z;
+
+
+};
+
+#endif // MACGRID_H
